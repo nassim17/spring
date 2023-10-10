41e32ac71490768366359ee5eff89cf1246ec7a6
nabed <nassim.abed@soprasteria.com>
2023-10-10 22:21:31 +0200

    Init transactionnal

---
+++ transactionnal/src/main/java/com/spring/transactionnal/TransactionnalApplication.java
@@ -0,0 +1,13 @@
+package com.spring.transactionnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionnalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionnalApplication.class, args);
+	}
+
+}
---
+++ transactionnal/src/test/java/com/spring/transactionnal/TransactionnalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.transactionnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionnalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
