08b99834baf1101cfac138b5c1610c23d5663db2
nabed <abednassim92@gmail.com>
2023-10-27 16:50:20 +0200

    SSE in Spring Web - init Server

---
+++ server-sent-events/sse-in-spring-mvc/src/main/java/com/spring/sseinspringmvc/SseInSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sseinspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseInSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseInSpringMvcApplication.class, args);
+	}
+
+}
---
+++ server-sent-events/sse-in-spring-mvc/src/test/java/com/spring/sseinspringmvc/SseInSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sseinspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseInSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
