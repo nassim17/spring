2ca39b418797e683bcdf3d1a7a5fec3c5436ce51
nabed <abednassim92@gmail.com>
2024-01-25 15:46:51 +0100

    validation: create ValidateRequestBodyController for démo.

---
+++ validation/src/main/java/com/spring/validation/controller/ValidateRequestBodyController.java
@@ -0,0 +1,22 @@
+package com.spring.validation.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.validation.dto.PayLoadDto;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/validation/validateBody")
+public class ValidateRequestBodyController {
+
+    @PostMapping("/valid")
+    ResponseEntity<String> validateBody(@Valid @RequestBody PayLoadDto payLoadDto) {
+        return ResponseEntity.ok("valid");
+    }
+}
