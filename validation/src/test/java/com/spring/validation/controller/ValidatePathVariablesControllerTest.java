fb1ac25e579da7fb309eecb85292eaf19d98ef19
nabed <abednassim92@gmail.com>
2024-01-30 16:24:15 +0100

    validation: create ValidatePathVariablesController for démo.

---
+++ validation/src/test/java/com/spring/validation/controller/ValidatePathVariablesControllerTest.java
@@ -0,0 +1,35 @@
+package com.spring.validation.controller;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ValidatePathVariablesControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void invalidRequestParameters() throws Exception {
+
+        mvc.perform(get("/validation/path-variables/3"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void validRequestParameters() throws Exception {
+
+        mvc.perform(get("/validation/path-variables/5"))
+                .andExpect(status().isOk());
+    }
+}
