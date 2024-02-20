1561623b7921c5be7c661102dec8d9084100664d
nabed <abednassim92@gmail.com>
2024-02-20 16:44:37 +0100

    ArchUnit: init Project

---
+++ archunit/src/test/java/com/spring/archunit/ArchunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.archunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
