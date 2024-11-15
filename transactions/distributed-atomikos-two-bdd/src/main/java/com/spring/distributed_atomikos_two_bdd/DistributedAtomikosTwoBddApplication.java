29c68b07562155bf060c4eacd1d2a57496b6bcd3
nabed <abednassim92@gmail.com>
2024-11-15 21:22:55 +0100

    distributed-atomikos-two-bdd: Init Project

---
+++ transactions/distributed-atomikos-two-bdd/src/main/java/com/spring/distributed_atomikos_two_bdd/DistributedAtomikosTwoBddApplication.java
@@ -0,0 +1,13 @@
+package com.spring.distributed_atomikos_two_bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAtomikosTwoBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedAtomikosTwoBddApplication.class, args);
+	}
+
+}
---
+++ transactions/distributed-atomikos-two-bdd/src/test/java/com/spring/distributed_atomikos_two_bdd/DistributedAtomikosTwoBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.distributed_atomikos_two_bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedAtomikosTwoBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
