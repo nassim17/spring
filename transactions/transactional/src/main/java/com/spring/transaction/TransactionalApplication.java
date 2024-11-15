a20c0e494d4f20887aff6224eb344a31a0a0f7c8
nabed <abednassim92@gmail.com>
2024-11-15 16:12:24 +0100

    Transactions: rename transaction to transactional.

--- transactions/transactional/src/main/java/com/spring/transaction/TransactionalApplication.java
+++ transactions/transactional/src/main/java/com/spring/transaction/TransactionalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransactionApplication {
+public class TransactionalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TransactionApplication.class, args);
+		SpringApplication.run(TransactionalApplication.class, args);
 	}
 
 }
--- transactions/transactional/src/test/java/com/spring/transaction/TransactionalApplicationTests.java
+++ transactions/transactional/src/test/java/com/spring/transaction/TransactionalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TransactionApplicationTests {
+class TransactionalApplicationTests {
 
 	@Test
 	void contextLoads() {
